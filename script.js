90c9e8f10e4d032de34a11b975d0725709870c12
sne-syn <snezhana.syngur@gmail.com>
2021-09-05 18:20:57 +0200

    Update volume

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const playBtn = document.getElementById('play-btn');
 const volumeIcon = document.getElementById('volume-icon');
 const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
-const currentTime = document.querySelector('time-elapsed');
+const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
@@ -26,14 +26,58 @@ const togglePlay = () => {
   }
 }
 
-
 // Progress Bar ---------------------------------- //
 
+const displayTime = (time) => {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+};
 
+const updateProgress = () => {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
+}
 
+const setProgress = (evt) => {
+  const newTime = evt.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+}
 // Volume Controls --------------------------- //
 
+const changeVolume = (evt) => {
+  let volume = evt.offsetX / volumeRange.offsetWidth;
+  if (volume < 0.1) {
+    volume = 0;
+  }
 
+  if (volume > 0.9) {
+    volume = 1;
+  }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+  volumeIcon.className = '';
+
+  switch (true) {
+    case volume > 0.7:
+      console.log(volume)
+      volumeIcon.classList.add('fas', 'fa-volume-up');
+      break;
+    case volume < 0.7 && volume > 0:
+      console.log(volume)
+      volumeIcon.classList.add('fas', 'fa-volume-down');
+      break;
+    case volume === 0:
+      console.log(volume)
+      volumeIcon.classList.add('fas', 'fa-volume-off');
+      break;
+    default:
+      volumeIcon.className = '';
+  }
+}
 
 // Change Playback Speed -------------------- //
 
@@ -46,3 +90,7 @@ const togglePlay = () => {
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('ended', showPlayIcon)
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
